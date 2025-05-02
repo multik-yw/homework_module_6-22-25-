a78053c232225323d1b296139c2cd73837774fe3
Alina Moskvina <alisevenmo@gmail.com>
2025-05-02 13:01:12 +0300

    homework done

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        print(f"Здравствуйте, {name}! Мы свяжемся с вами по номеру телефона {phone}")
+        return HttpResponse(f"Здравствуйте, {name}! Мы свяжемся с вами по номеру телефона {phone}")
+    return render(request, 'catalog/contacts.html')
