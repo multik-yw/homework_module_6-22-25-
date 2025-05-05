0c314281a1c5860f375b017ac35a02d3a900cd89
Alina Moskvina <alisevenmo@gmail.com>
2025-05-05 15:29:43 +0300

    fixes

---
+++ management/commands/__init__.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Category, Product
+from django.core.management import call_command
+
+class Command(BaseCommand):
+    help = 'Load test data from fixture'
+
+    def handle(self, *args, **kwargs):
+        Category.objects.all().delete()
+        Product.objects.all().delete()
+
+        call_command('loaddata', 'catalog_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
